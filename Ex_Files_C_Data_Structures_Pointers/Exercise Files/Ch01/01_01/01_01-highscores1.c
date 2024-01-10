80d156c8068749e02435bd0088b327a7091bf00a
gondhi <an698849@dal.ca>
2024-01-10 19:24:58 -0400

    "practicing array assignment"

--- Ex_Files_C_Data_Structures_Pointers/Exercise Files/Ch01/01_01/01_01-highscores1.c
+++ Ex_Files_C_Data_Structures_Pointers/Exercise Files/Ch01/01_01/01_01-highscores1.c
@@ -2,20 +2,16 @@
 
 int main()
 {
-	float highscore1,highscore2,highscore3,highscore4,highscore5;
+	float highscore[5];
 
-	highscore1 = 993.86;
-	highscore2 = 682.01;
-	highscore3 = 639.60;
-	highscore4 = 310.45;
-	highscore5 = 123.25;
-
+    for (int i = 0; i < 5; ++i) {
+        printf("High score #%d ",i+1);
+        scanf("%f",&highscore[i]);
+    }
 	puts("Here are the high scores:");
-	printf("#1 %.2f\n",highscore1);
-	printf("#2 %.2f\n",highscore2);
-	printf("#3 %.2f\n",highscore3);
-	printf("#4 %.2f\n",highscore4);
-	printf("#5 %.2f\n",highscore5);
+    for (int i = 0; i < 5; ++i) {
+        printf("#%d %.2f\n",i+1,highscore[i]);
+    }
 
 	return(0);
 }
